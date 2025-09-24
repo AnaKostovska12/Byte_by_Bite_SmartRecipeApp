4322c45da73934f6cb3a117e7a60a6cc9963ad8c
AnaKostovska12 <kostovskaana360@gmail.com>
2025-09-25 00:19:56 +0200

    Update settings.py

--- smart_recipe/settings.py
+++ smart_recipe/settings.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'smart_recipe.urls'
@@ -83,5 +84,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
